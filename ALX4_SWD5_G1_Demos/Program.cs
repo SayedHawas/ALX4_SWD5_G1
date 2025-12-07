d7c8c64668d50e79558eed36bbfa11f02655b417
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2025-12-07 23:08:56 +0200

    Add project files.

---
+++ ALX4_SWD5_G1_Demos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ALX4_SWD5_G1_Demos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
