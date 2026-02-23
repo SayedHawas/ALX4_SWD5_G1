966fb81b8997bf83b0e74af3ae1944cce8ec988d
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2026-02-24 00:08:41 +0200

    update

--- Day1WinFormsApp/Form1.cs
+++ Day1WinFormsApp/Form1.cs
@@ -11,5 +11,27 @@ namespace Day1WinFormsApp
         {
             MessageBox.Show("Welcome " + textBox1.Text);
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //DataGridView
+            dataGridView1.DataSource = new List<Person>
+            {
+                new Person { Name = "Alice", Age = 30 },
+                new Person { Name = "Bob", Age = 25 },
+                new Person { Name = "Charlie", Age = 35 }
+            };
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.Yellow;
+        }
+    }
+
+    internal class Person
+    {
+        public string Name { get; internal set; }
+        public int Age { get; internal set; }
     }
 }
