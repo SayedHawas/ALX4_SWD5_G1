df4f9a3a0fff5f7efc31139e93e59f7f4154f0d3
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2026-02-27 17:34:08 +0200

    Update

---
+++ Day22SerilizationDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.Json;
+
+namespace Day22SerilizationDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Person person = new Person { Name = "John Doe", Age = 30 };
+
+            //// Serialize the object to a file
+            //using (FileStream fs = new FileStream("person.dat", FileMode.Create))
+            //{
+            //    BinaryFormatter formatter = new BinaryFormatter();
+            //    formatter.Serialize(fs, person);
+            //}
+
+            //// Deserialize the object from the file
+            //using (FileStream fs = new FileStream("person.dat", FileMode.Open))
+            //{
+            //    BinaryFormatter formatter = new BinaryFormatter();
+            //    Person deserializedPerson = (Person)formatter.Deserialize(fs);
+            //    Console.WriteLine($"Name: {deserializedPerson.Name}, Age: {deserializedPerson.Age}");
+            //}
+
+            //stream 
+            //XML 
+            //using (FileStream fs = new FileStream("person.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            //{
+            //    XmlSerializer serializer = new XmlSerializer(typeof(Person));
+            //    serializer.Serialize(fs, person);
+            //}
+
+            //// De-serialize the object from the file
+            //using (FileStream fs = new FileStream("person.xml", FileMode.Open))
+            //{
+            //    XmlSerializer serializer = new XmlSerializer(typeof(Person));
+
+            //    Person deserializedPerson = (Person)serializer.Deserialize(fs);
+            //    Console.WriteLine($"Name: {deserializedPerson.Name}, Age: {deserializedPerson.Age}");
+            //}
+
+
+            // Serialize the object to a JSON string
+            string jsonString = JsonSerializer.Serialize(person);
+            File.WriteAllText("person.json", jsonString);
+
+            // Deserialize the object from the JSON string
+            string jsonStringFromFile = File.ReadAllText("person.json");
+            Person deserializedPerson = JsonSerializer.Deserialize<Person>(jsonStringFromFile);
+            Console.WriteLine($"Name: {deserializedPerson.Name}, Age: {deserializedPerson.Age}");
+
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
