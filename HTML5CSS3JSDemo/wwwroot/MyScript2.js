f5a2c3380d81d5fea7565dc8094ebc4e05ed381f
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2026-03-03 13:10:39 +0200

    Update

---
+++ HTML5CSS3JSDemo/wwwroot/MyScript2.js
@@ -0,0 +1,27 @@
+
+      // window.alert("Welcome in Js From External file ....");
+
+      //document.getElementById('para').innerHTML+='<h1 style=color:blue;> Welcome DOM </h1>'
+      function factorial(Number)
+      {
+            if(Number<1)
+                return 0;
+            if(Number==1)
+                return 1;
+            else 
+                return Number *factorial(Number-1);
+      }
+
+      //Show From return
+      function ShowResult(inputId , valueResult)
+      {
+        document.getElementById(inputId).innerText =  valueResult;
+      }
+
+      //get value 
+      function GetValue(inputId)
+      {
+        var x = document.getElementById(inputId).value;
+          return x
+      }
+
